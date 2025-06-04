0ec813048a9a284497e6fba68c37e1f8ca202e87
Phu Binh Dang <Phu.Binh.Dang@wps.de>
2025-06-04 22:04:59 +0200

    add DOWNLOAD_RESOLUTION env param

--- src/tools/youtube_downloader.py
+++ src/tools/youtube_downloader.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from pytubefix import YouTube
 from util.persistent_stqdm import PersistentSTQDM
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -33,12 +34,13 @@ class YoutubeDownloader:
             self.streamlit_tqdm.refresh()
 
     def download(self, video_url) -> VideoInfo:
+        download_res = os.getenv("DOWNLOAD_RESOLUTION", "720p")
         yt = YouTube(video_url, on_progress_callback=self.on_progress)
         title = self.clean_title(yt.title)
         video_file_path = None
         audio_file_path = None
         try:
-            video_file_path = self.download_video(yt, title)
+            video_file_path = self.download_video(yt, title, resolution=download_res)
         except Exception as e:
             logger.error(f"Error while downloading video from {video_url}: {e}")
         if not self.video_only:
@@ -47,21 +49,30 @@ class YoutubeDownloader:
             except Exception as e:
                 logger.error(f"Error while downloading audio from {video_url}: {e}")
 
-        return VideoInfo(url=video_url,
-                         title=title,
-                         video_path=video_file_path,
-                         audio_path=audio_file_path)
+        return VideoInfo(
+            url=video_url,
+            title=title,
+            video_path=video_file_path,
+            audio_path=audio_file_path,
+        )
 
     def download_audio(self, yt: YouTube, title: str) -> str:
         try:
             audio_stream = yt.streams.filter(progressive=False, type="audio").first()
             download_path = Path.cwd() / "data" / f"{title}" / "input"
             download_path.mkdir(parents=True, exist_ok=True)
-            self.streamlit_tqdm = PersistentSTQDM(total=audio_stream.filesize, unit="B", unit_scale=True,
-                                                  desc="Downloading AUDIO",
-                                                  backend=self.show_progress_in_backend)
-            audio_file_path = audio_stream.download(output_path=str(download_path), filename="audio.mp3",
-                                                    skip_existing=False)
+            self.streamlit_tqdm = PersistentSTQDM(
+                total=audio_stream.filesize,
+                unit="B",
+                unit_scale=True,
+                desc="Downloading AUDIO",
+                backend=self.show_progress_in_backend,
+            )
+            audio_file_path = audio_stream.download(
+                output_path=str(download_path),
+                filename="audio.mp3",
+                skip_existing=False,
+            )
             if not audio_file_path:
                 raise Exception()
             return audio_file_path
@@ -69,24 +80,37 @@ class YoutubeDownloader:
             raise e
 
     def clean_title(self, title) -> str:
-        title = re.sub(r'[^A-Za-z0-9\s]', '', title)
-        return re.sub(r'\s+', '_', title)
+        title = re.sub(r"[^A-Za-z0-9\s]", "", title)
+        return re.sub(r"\s+", "_", title)
 
-    def download_video(self, yt: YouTube, title, resolution="1080p") -> str:
+    def download_video(self, yt: YouTube, title, resolution="720p") -> str:
         try:
-            video_streams = yt.streams.filter(progressive=False, type="video").order_by('resolution').desc()
+            video_streams = (
+                yt.streams.filter(progressive=False, type="video")
+                .order_by("resolution")
+                .desc()
+            )
             video_stream = video_streams.filter(resolution=resolution).first()
-            if not video_stream: 
+            if not video_stream:
                 video_stream = video_streams.first()
-                logger.info(f"Stream of resolution {resolution} not found, download stream of {video_stream.resolution} instead")
+                logger.info(
+                    f"Stream of resolution {resolution} not found, download stream of {video_stream.resolution} instead"
+                )
             # Create a Streamlit progress bar widget
             download_path = Path.cwd() / "data" / f"{title}" / "input"
             download_path.mkdir(parents=True, exist_ok=True)
-            self.streamlit_tqdm = PersistentSTQDM(total=video_stream.filesize, unit="B", unit_scale=True,
-                                                  desc="Downloading VIDEO",
-                                                  backend=self.show_progress_in_backend)
-            video_file_path = video_stream.download(output_path=str(download_path), filename="video.mp4",
-                                                    skip_existing=False)
+            self.streamlit_tqdm = PersistentSTQDM(
+                total=video_stream.filesize,
+                unit="B",
+                unit_scale=True,
+                desc="Downloading VIDEO",
+                backend=self.show_progress_in_backend,
+            )
+            video_file_path = video_stream.download(
+                output_path=str(download_path),
+                filename="video.mp4",
+                skip_existing=False,
+            )
             return video_file_path
         except Exception as e:
             raise e
